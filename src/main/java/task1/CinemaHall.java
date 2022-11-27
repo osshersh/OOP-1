72833ab5b2c8a22ec2dca1baab930aa15f1f25b8
osshersh <olegshershnov@gmail.com>
2022-11-27 18:39:34 +0100

    Zrobiono zadanie OOP1 task1

---
+++ src/main/java/task1/CinemaHall.java
@@ -0,0 +1,14 @@
+package task1;
+
+public class CinemaHall {
+
+    private int seatNumber;
+    private int rowNumber;
+    private String statusSeat;
+
+    public CinemaHall(int seatNumber, int rowNumber, String statusSeat){
+        this.seatNumber = seatNumber;
+        this.rowNumber = rowNumber;
+        this.statusSeat = statusSeat;
+    }
+}
