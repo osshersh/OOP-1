314263e4aac563d88fc3a160d54c14ef413c3876
osshersh <olegshershnov@gmail.com>
2022-12-01 06:52:19 +0100

    Zmieniono nazwe metody w task4 i nazwy metod w testach task4

--- src/main/java/task4/Conversion.java
+++ src/main/java/task4/Conversion.java
@@ -3,7 +3,7 @@ package task4;
 public class Conversion {
     private final static double SECOND_IN_MINUTE = 60;
 
-    public static double conversionMinToSecond(int value){
+    public static double convertMinToSecond(int value){
         return value * SECOND_IN_MINUTE;
     }
 }
--- src/test/java/task4/ConversionTest.java
+++ src/test/java/task4/ConversionTest.java
@@ -8,14 +8,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class ConversionTest {
 
     @Test
-    void shouldOkThatConversionMinToSecond() {
-        double seconds = Conversion.conversionMinToSecond(10);
+    void shouldReturnProperWhenConvertMinInSecond() {
+        double seconds = Conversion.convertMinToSecond(10);
         Assertions.assertEquals(600,seconds);
     }
 
     @Test
-    void shouldOkThatIsNotConversionMinToSecond(){
-        double second = Conversion.conversionMinToSecond(10);
+    void shouldReturnProperWhenIsNotConvertMinInSecond(){
+        double second = Conversion.convertMinToSecond(10);
         Assertions.assertNotEquals(601,second);
     }
 }
